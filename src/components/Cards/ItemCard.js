a74eee7809996fd25b62d56711f0e9ef40eb2323
avinash6982 <avinashghoshpullur@gmail.com>
2023-04-23 23:00:28 +0400

    WIP: add to cart and cart listings

--- src/components/Cards/ItemCard.js
+++ src/components/Cards/ItemCard.js
@@ -43,6 +43,7 @@ const ItemCard = ({
         </div>
         <div className="flex items-center justify-between">
           <span className="text-md font-bold text-gray-900">${price}</span>
+          {/* Add action */}
           <a
             href="#"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
